5037ef93ae9e9497304a0f6bf1defddaa35126dd
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-06-26 18:48:32 -0700

    Declare various and initialize

---
+++ Java/Data10.java
@@ -0,0 +1,23 @@
+class Data10 {
+    public static void main(String[] ar) {
+        int i;
+        int[] a = new int [] {1,2,3,4};
+        int[] b = {1,2,3,4};
+        int c[];
+        c = new int [] {1,2,3,4};
+        int d[] = new int [] {1,2,3,4};
+        int e[] = {1,2,3,4};
+        int f[];
+        f = new int [] {1,2,3,4};
+
+        for(i = 0; i < 4; i++) {
+            System.out.print(a[i]);
+            System.out.print(b[i]);
+            System.out.print(c[i]);
+            System.out.print(d[i]);
+            System.out.print(e[i]);
+            System.out.print(f[i]);
+        }
+    }
+
+}
