a1f6197f2460d335c4a083397f45896a60ca103f
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-06 01:49:16 -0700

    Printf function

---
+++ Java/Printf.java
@@ -0,0 +1,15 @@
+class Printf {
+  public static void main(String[] ar) {
+   System.out.printf("%d ",3);
+   System.out.printf("%4d ",3);
+   System.out.printf("%4.1f ",44.23);
+   System.out.printf("%04d ",34);
+   System.out.printf("%04.1f ",2.34);
+
+  }
+
+  //%d int
+  //%f double/float
+  //%c charactor
+  //%s string
+}
