6923c90624b5f9079cb51e089da8c3d616b34a4b
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-14 13:32:47 -0700

    Make Score class

---
+++ Java/Score.java
@@ -0,0 +1,16 @@
+class Student {
+  String name;
+  int [] point = new int[3];
+
+  double eve() {
+    return (100.0);
+  }
+}
+
+
+class Score {
+  public static void main(String[] arg) {
+    Student kim = new Student();
+    Student lee = new Student();
+  }
+}
