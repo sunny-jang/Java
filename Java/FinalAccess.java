0e12afa36c27a40e086c6784fa3855593db0d42d
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-16 03:25:55 -0700

    Final access

---
+++ Java/FinalAccess.java
@@ -0,0 +1,19 @@
+final class Animal {
+  final int a = 3;
+}
+
+// class Cat extends Animal{
+
+// }
+
+
+public class FinalAccess {
+
+  public static void main(String[] args) {
+
+    Animal dog = new Animal();
+    // Cat mu = new Cat();
+
+    // dog.a = 1;
+  }
+}
