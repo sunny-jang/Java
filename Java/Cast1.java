d08ff7a9a248286fbf252efa2ea2315a897f15d4
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-21 00:43:08 -0700

    Cast opperation

--- Java/Cast1.java
+++ Java/Cast1.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;
-class Bissextile {
+class Cast1 {
   public static void main(String[] ar) {
     System.out.println("정수/정수 =  3/2 = " + 3/2);
     System.out.println("실수/정수 =  3/2 = " + (float)3/2);
